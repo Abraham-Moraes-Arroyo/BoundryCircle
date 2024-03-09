98a670079dd874fd4af616fb7c456b08fb20a56e
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-03-09 11:33:57 -0600

    Initial Commit

---
+++ BoundryCircle/BoundryCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoundryCircleApp.swift
+//  BoundryCircle
+//
+//  Created by Abraham Morales Arroyo on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BoundryCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
